b61d409d94b1809e69d80dff81a1d0ee9fce1f1f
faizan1122786666 <faizan9664740@gmail.com>
2025-07-11 08:11:35 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const contactRoute = require('./routes/contact');
-const checkoutRoute = require('./routes/checkout');
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors({
-  origin: 'https://watchcollection001.netlify.app', // ✅ your Netlify domain
-  methods: ['POST'],
-  credentials: true
-}));
-
-
+app.use(cors());
 app.use(express.json());
-
-// Routes
 app.use('/api/contact', contactRoute);
-app.use('/api/checkout', checkoutRoute);  // ✅ These must come BEFORE app.listen()
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-
